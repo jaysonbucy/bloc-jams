0ad575d250d6595e17caa7b78303cd6a0159653b
jaysonbucy <jaysonbucy@legalshieldcorp.com>
2016-10-26 11:24:57 -0500

    assignment 13 DOM Scripting: play/pause 2

--- scripts/album.js
+++ scripts/album.js
@@ -80,9 +80,13 @@ var setCurrentAlbum = function(album) {
 var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
-        while (currentParent.className != targetClass && currentParent.className !== null) {
-            currentParent = currentParent.parentElement;
-        }
+        if (currentParent != null){
+            if (currentParent.className != targetClass && currentParent.className !== null) {
+                currentParent = currentParent.parentElement;
+            } else if (currentParent.className == null){
+                console.log("No parent found with that class name.")
+            }
+        } else {console.log("No parent found.")}
         return currentParent;
     }
 };
