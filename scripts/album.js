b2b3d99aaa2008d7b7f275ad3de48454b1341015
jaysonbucy <jaysonbucy@legalshieldcorp.com>
2016-11-10 07:42:38 -0600

    assignment 20 wip

--- scripts/album.js
+++ scripts/album.js
@@ -113,13 +113,8 @@ var previousSong = function() {
 
 var setSong = function(songNumber){
   if (currentSoundFile) {
-<<<<<<< HEAD
-         currentSoundFile.stop();
-    }
-=======
     currentSoundFile.stop();
   }
->>>>>>> assignment-20-buzz-library
   currentlyPlayingSongNumber = parseInt(songNumber);
   currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
 
@@ -148,55 +143,6 @@ var updatePlayerBarSong = function(){
 };
 
 var clickHandler = function() {
-<<<<<<< HEAD
-	var songNumber = parseInt($(this).attr('data-song-number'));
-
-	if (currentlyPlayingSongNumber !== null) {
-		var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
-		currentlyPlayingCell.html(currentlyPlayingSongNumber);
-	}
-	if (currentlyPlayingSongNumber !== songNumber) {
-	   $(this).html(pauseButtonTemplate);
-	    setSong(songNumber);
-      currentSoundFile.play();
-      updatePlayerBarSong();
-	} else if (currentlyPlayingSongNumber === songNumber) {
-    if (currentSoundFile.isPaused()) {
-      $(this).html(pauseButtonTemplate);
-<<<<<<< HEAD
-      $('.main-controls .play-pause').html(playerBarPauseButton);
-      currentSoundFile.play();
-    } else {
-      $(this).html(playButtonTemplate);
-      $('.main-controls .play-pause').html(playerBarPlayButton);
-      currentSoundFile.pause();
-    }
-  }
-=======
-      $playPauseButton.html(playerBarPauseButton);
-      currentSoundFile.play();
-    } else {
-      $(this).html(playButtonTemplate);
-      $playPauseButton.html(playerBarPlayButton);
-      currentSoundFile.pause();
-    }
-  }
-};
-
-var togglePlayFromPlayBar = function(){
-  var $nextSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
-
-  if(currentSoundFile.isPaused()){
-    $(this).html(playPauseButton);
-    $nextSongNumberCell.html(pauseButtonTemplate);
-    currentSoundFile.play();
-  } else {
-    $(this).html(playPauseButton);
-    $nextSongNumberCell.html(playButtonTemplate);
-    currentSoundFile.pause();
-  }
->>>>>>> assignment-20-buzz-library
-=======
 
     var songNumber = parseInt($(this).attr('data-song-number'));
 
@@ -236,7 +182,6 @@ var togglePlayFromPlayerbar = function() {
        $(this).html(playerBarPlayButton);
        currentSoundFile.pause();
    }
->>>>>>> assignment-20-buzz-library
 };
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
@@ -245,31 +190,18 @@ var playerBarPlayButton = '<span class="ion-play"></span>';
 var playerBarPauseButton = '<span class="ion-pause"></span>';
 
 var currentAlbum = null;
-<<<<<<< HEAD
-var $previousButton = $('.main-controls .previous');
-var $nextButton = $('.main-controls .next');
-var $playPauseButton = $('.main-controls .play-pause');
-=======
->>>>>>> assignment-20-buzz-library
 var currentlyPlayingSongNumber = null;
 var currentSongFromAlbum = null;
 var currentSoundFile = null;
 var currentVolume = 80;
-<<<<<<< HEAD
-=======
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
 var $playPauseButton = $('.main-controls .play-pause');
->>>>>>> assignment-20-buzz-library
 
 $(document).ready(function() {
      setCurrentAlbum(albumPicasso);
      $previousButton.click(previousSong);
      $nextButton.click(nextSong);
-<<<<<<< HEAD
-     $playPauseButton.click(togglePlayFromPlayBar);
-=======
      $playPauseButton.click(togglePlayFromPlayerbar);
->>>>>>> assignment-20-buzz-library
  });
